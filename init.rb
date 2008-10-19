4f579b980a4887f836a19e0ef00eae3f1297e3e9
eno <eno@capman.lan>
2008-10-19 17:30:36 +0200

    Deactives the plugin, because it got superseded by http://github.com/pboy/with_3rd_party

--- init.rb
+++ init.rb
@@ -1,12 +1,6 @@
 class ActionController::Base
-
   class_inheritable_accessor :urchin_id
 
   def self.with_urchin_tracker(urchin_id)
-    with_bodywork
-    include Eno::UrchinTracker 
-    
-    after_filter :add_urchin_tracker
-    self.urchin_id = urchin_id
   end
 end
